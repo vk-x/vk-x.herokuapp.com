84e0af53ef9beff9d30fe47395a6fe49e2bcaa2d
Nikita Litvin <deltaidea@derpy.ru>
2014-05-08 15:59:28 +0600

    feat(dist): use application/x-xpinstall type for .xpi

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.use(express.static(__dirname + '/dist'))
+app.use('/dist', function(request, response) {
+  response.type('application/x-xpinstall')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
