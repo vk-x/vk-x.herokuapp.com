812c02b79796c65ed8abc93e964c8acc03541fa7
Nikita Litvin <deltaidea@derpy.ru>
2014-05-09 05:47:31 +0600

    feat(dist): remove Firefox, send Opera as an attachment

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.use(express.static(__dirname + '/dist'))
-app.use('/dist', function(request, response) {
-  response.type('application/x-xpinstall')
+app.get('/opera', function(request, response) {
+  response.attachment('https://github.com/vk-x/vk-x/releases/download/v0.5.0/vk-x-0.5.0-opera.oex')
+  response.type('application/x-opera-extension')
+  response.send()
 })
 
 app.listen(app.get('port'), function() {
