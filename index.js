d467ceefe6ae84d8cfcbea3353b12fec2d232972
Nikita Litvin <deltaidea@derpy.ru>
2014-05-09 07:13:25 +0600

    refactor(common): remove excess things

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
 
 app.get('/opera', function(request, response) {
   response.attachment('https://github.com/vk-x/vk-x/releases/download/v0.5.0/vk-x-0.5.0-opera.oex')
